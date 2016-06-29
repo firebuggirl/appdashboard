7a555d5850a0382416a8749210fbab1b30e84db3
Juliette <julietteredheadgirl@yahoo.com>
2016-06-29 15:01:48 -0500

    Additional click functions for messagesent.js

--- js/app.js
+++ js/app.js
@@ -11600,6 +11600,7 @@ $(document).ready(function () {
 
 var $overlay = $('<div id="overlay"></div>');
 var $messageSent = $("#messageSent");
+var $submitButton =('#submitButton');
 //var $caption = $("<p></p>");
 
 //An message to overlay
@@ -11633,12 +11634,72 @@ $("#messageButton").click(function (event) {
 
 //When close button is clicked hide the overlay, re-introduce elements
 
-
 $("#closeOverlay").click(function () {
 
     $overlay.hide();
     $("header").show();
     $('#wrapper').show();
+
+});
+
+
+/* Show message that confirms saved settings */
+$("#submitButton").click(function (event) {
+
+
+    event.preventDefault();//prevent default browser behavior
+
+    $("header").hide();
+  //Show the overlay.
+    $overlay.show();
+    $messageSent.html("Your settings have been saved!");
+    $messageSent.css("color", "#ffffff");
+    $messageSent.css("fontSize", "26px");
+    $messageSent.show();
+
+    $('.main-nav ul li').hide();
+    $('#wrapper').hide();
+
+    $messageSent.click(function () {/* Use messageSent click function locally to change
+                                     the inner HTML of $messageSent om #submitButton independently of
+                                     #messageButton click function, then reload the page to go back to original inner HTML*/
+        $overlay.hide();
+        $("header").show();
+        $('#wrapper').show();
+        location.reload();
+
+    });
+
+
+});
+
+
+/* Show message that confirms saved settings */
+$("#cancelButton").click(function (event) {
+
+
+    event.preventDefault();//prevent default browser behavior
+
+    $("header").hide();
+  //Show the overlay.
+    $overlay.show();
+    $messageSent.html("Your settings have been reset!");
+    $messageSent.css("color", "#ffffff");
+    $messageSent.css("fontSize", "26px");
+    $messageSent.show();
+
+    $('.main-nav ul li').hide();
+    $('#wrapper').hide();
+
+    $messageSent.click(function () {
+
+        $overlay.hide();
+        $("header").show();
+        $('#wrapper').show();
+        location.reload();
+
+    });
+
 
 });
 
@@ -11651,8 +11712,6 @@ $(document).ready(function() {
 
 $('#submitButton').click(function() { /* currently resets all LS data*/
   localStorage.timezone = $("#timezone").val();
-  //localStorage.checkbox = $('input[type=radio]:checked').val();
-  //localStorage.checkbox2 = $('input[type=radio]:checked').val();
   localStorage.checkbox = $('#checkbox').val();
   localStorage.checkbox2 = $('#checkbox2').val();
 });
@@ -11660,10 +11719,11 @@ $('#submitButton').click(function() { /* currently resets all LS data*/
 function loadSettings() {
 
     $('input[value="' + localStorage.checkbox + '"]').prop('checked', true);
-      $('input[value="' + localStorage.checkbox2 + '"]').prop('checked', true);
+    $('input[value="' + localStorage.checkbox2 + '"]').prop('checked', true);
     $("#timezone").val(localStorage.timezone);
 }
 loadSettings();
+
 function saveSettings() {
 
     localStorage.timezone = $("#timezone").val();
